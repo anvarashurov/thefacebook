9d60117b4012ffb810e1b14244ba50556cb3a25e
antox7 <anvar.ashurov84@qmail.cuny.edu>
2019-10-01 14:32:09 -0400

    Complete figvaper

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+    # create a new user with given params
+    @user = User.new(user_params)
+    if @user.save
+      #success register / possibly verify
+      render json: @user
+      # render json: "Success Register"
+    else
+      # render json: "Error"
+      render json: @user.errors.full_messages
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :password, :birth_date,
+    :gender, :email, :phone_number)
+  end
+end
