c14b3f5c92da85b13b2e6258b7085c9f994332d8
antox7 <anvar.ashurov84@qmail.cuny.edu>
2019-10-01 14:34:12 -0400

    Set up user and session routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'static_pages#root'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :create]
+  resource :session, only: [:new, :create, :destroy]
 end
