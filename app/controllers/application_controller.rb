a5b313b414430cddf09c8d8976591cdc10840b37
antox7 <anvar.ashurov84@qmail.cuny.edu>
2019-10-01 15:04:33 -0400

    Implement CELLL

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
 class ApplicationController < ActionController::Base
   # C E L L L
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def ensure_logged_in
+    # change path unless logged in
+    render json: "Redirecting to sign up page" unless logged_in?
+  end
+  
+  def logged_in?
+    !!current_user
+  end
+
+  def login!(user)
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def logout!
+    current_user.reset_session_token!
+    @current_user = nil
+    session[:session_token] = nil    
+  end
+
 end
