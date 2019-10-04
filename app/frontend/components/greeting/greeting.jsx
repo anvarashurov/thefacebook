172322c20fd97167228c303c88727a14e545284e
antox7 <anvar.ashurov84@qmail.cuny.edu>
2019-10-04 11:42:03 -0400

    Complete body left child styling. Work on Footer

--- app/frontend/components/greeting/greeting.jsx
+++ app/frontend/components/greeting/greeting.jsx
@@ -26,8 +26,51 @@ class Greeting extends React.Component {
           <Header />
           <div className="landing_page_body">
             <div className="body_upper_part">
-              <SignupFormContainer />
-              {/* Here goes the Left Child of Body */}
+              <div className="body_top_left_child">
+                <h1>Connect with friends and the</h1>
+                <h1>world around you on Facebook.</h1>
+                <table className="body_upper_left_table">
+                 <tbody>
+                  <tr>
+                    <td className="top_left_row">
+                      <span className="img1">
+                        <img src={window.top_left_1} />
+                      </span>
+                      {/* REMOVE IF NOT NEEDED */}
+                      <span className="body_upper_left_text_span">
+                          <h4>See photos and updates</h4> <p> from friends in News Feed.</p>
+                      </span>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="top_left_row">
+                      <span className="img2">
+                        <img src={window.top_left_2} />
+                      </span> 
+                        <span className="body_upper_left_text_span">
+                          <h4>Share what's new</h4> <p>in your life on your Timeline.</p>
+                      </span>
+                    </td>
+                  </tr>
+                  <tr>
+                      <td className="top_left_row">
+                        <span className="img3">
+                          <img src={window.top_left_3} />
+                        </span> 
+                        <span className="body_upper_left_text_span">
+                        <h4 >Find more</h4> <p> of what you're looking for with Facebook Search.</p>
+                        </span>
+                    </td>
+                  </tr>
+                  </tbody>   
+                </table>
+               
+                
+                
+              </div>
+              <div className="body_top_right_child">
+                <SignupFormContainer />
+              </div>
             </div>
             <div className="create_page_message">
               <p>
