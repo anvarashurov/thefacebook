f65426963faacf55bd55309ca40b907faae7527c
antox7 <anvar.ashurov84@qmail.cuny.edu>
2019-11-01 13:26:25 -0400

    Add Friend back-end schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_26_163352) do
+ActiveRecord::Schema.define(version: 2019_11_01_150251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,14 @@ ActiveRecord::Schema.define(version: 2019_10_26_163352) do
     t.string "content", null: false
     t.integer "author_id", null: false
     t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "friends", force: :cascade do |t|
+    t.string "friend_sender", null: false
+    t.string "friend_receiver", null: false
+    t.boolean "status", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
