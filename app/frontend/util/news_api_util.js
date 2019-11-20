c6b7859bcc0ef9eea0cf62008d845d9e73d3f6b9
antox7 <anvar.ashurov84@qmail.cuny.edu>
2019-11-20 17:51:28 -0500

    Add last changes before Likes

---
+++ app/frontend/util/news_api_util.js
@@ -0,0 +1,7 @@
+// export const fetchNews = () => {
+//     return $.ajax({
+//         url: `https://newsapi.org/v2/top-headlines?country=us&apiKey=${window.newsApiKey}`
+//     })
+// }
+
+// // the key sits inside credentials: news => api_key => some random string of characters
